2d8e88e992dd78a3e53da4ae0ec2fb394ede9fc3
mperaic <mperaic@gmail.com>
2020-12-14 20:34:34 +0100

    add delete feature

--- public/browser.js
+++ public/browser.js
@@ -1,4 +1,19 @@
 document.addEventListener("click", function (e) {
+    // delete feature
+    if (e.target.classList.contains("delete-me")) {
+        if (confirm("Do you really want to delete this item?")) {
+            axios.post("/delete-item", {id: e.target.getAttribute("data-id")}).then(function() {
+                e.target.parentElement.parentElement.remove()
+            }).catch(function() {
+                console.log("Please try again!")
+            })
+
+        }
+
+    }
+
+
+    // update feature
     if (e.target.classList.contains("edit-me")) {
         let userInput = prompt("Enter your new text", e.target.parentElement.parentElement.querySelector(".item-text").innerHTML)
         if (userInput) {
